97c1da12bbcfcdd4d7550da9d7b9b2fe33fe7115
Vladimir Pavlovic <vlada.pavlovic0@gmail.com>
2020-10-11 21:18:03 +0200

    Issues monitoring - Sentry

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
