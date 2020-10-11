5ebce2f0b7a6bce689448e707706e3640e005558
Vladimir Pavlovic <vlada.pavlovic0@gmail.com>
2020-10-11 22:28:24 +0200

    migration fix

--- migrations/Version20201006174043.php
+++ migrations/Version20201006174043.php
@@ -20,7 +20,16 @@ final class Version20201006174043 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, registration_date DATE NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (
+                            id INT AUTO_INCREMENT NOT NULL,
+                            email VARCHAR(180) NOT NULL,
+                            roles LONGTEXT NOT NULL,
+                            password VARCHAR(255) NOT NULL, 
+                            first_name VARCHAR(255) NOT NULL, 
+                            last_name VARCHAR(255) NOT NULL, 
+                            registration_date DATE NOT NULL, 
+                            UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), 
+                            PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
